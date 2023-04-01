be28e2dd7a1f4781f4bfd07dbf5c7adbd9c8e314
Sriram Viswanathan <sriramvi@andrew.cmu.edu>
2023-04-01 15:25:50 -0400

    Removing doublePIR example - not working as expected

--- build.rs
+++ build.rs
@@ -13,12 +13,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .compile(
             &["protos/frodo_pir.proto"], &[""]
         ).unwrap();
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .out_dir("src/double_pir")
-        .compile(
-            &["protos/double_pir.proto"], &[""]
-        ).unwrap();
     Ok(())
 }
--- src/double_pir/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod double_pir_grpc {
-    tonic::include_proto!("double_pir_grpc");
-}
